7c11895b122eecd0997c09e91c8144d5640cec6c
Chananet <chananet7@gmail.com>
2025-07-27 11:01:16 +1000

    add os

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,9 +9,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    #print("Hello from ai-agent-p1!")
+    if len(sys.argv) == 1:
+        print("Usage: uv run main.py 'prompt' ")
+        sys.exit(1)
+
+    
+    
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+    model='gemini-2.0-flash-001', contents=sys.argv[1])
     usage = response.usage_metadata
     print(response.text)
     print(f"Prompt tokens: {usage.prompt_token_count}")
