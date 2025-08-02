024b38e9f3018ddb236f8f0b9d59eba1f56e6daf
Chananet <chananet7@gmail.com>
2025-08-02 13:26:55 +1000

    add argv and verbose

--- main.py
+++ main.py
@@ -2,25 +2,49 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+
+
+
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+
     if len(sys.argv) == 1:
         print("Usage: uv run main.py 'prompt' ")
         sys.exit(1)
 
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
+    generate_content(client,messages)
+
+
+
+
+
     
-    
+def generate_content(client,messages):
+    verbose = "--verbose" in sys.argv   
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1])
+    model='gemini-2.0-flash-001', contents=messages)
     usage = response.usage_metadata
+    
+
+    if verbose:
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
+    print("Response:")    
     print(response.text)
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+
+
 
 
 
