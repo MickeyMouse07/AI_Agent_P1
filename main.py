68330e7fe096258d69cb688d6e2f7aba15b5744d
Chananet <chananet7@gmail.com>
2025-11-08 10:48:19 +1100

    system prompt

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
-
-
-
+from prompts import *
 
 
 def main():
@@ -14,9 +11,13 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    
 
     if len(sys.argv) == 1:
-        print("Usage: uv run main.py 'prompt' ")
+        print("AI Code Assistant")
+        print("Usage: uv run main.py 'prompt' or ")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
     messages = [
@@ -24,18 +25,15 @@ def main():
     ]
 
     generate_content(client,messages)
-
-
-
-
-
     
 def generate_content(client,messages):
-    verbose = "--verbose" in sys.argv   
+    verbose = "--verbose" in sys.argv 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages)
+    model='gemini-2.0-flash-001', 
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
     usage = response.usage_metadata
-    
 
     if verbose:
         print(f"User prompt: {sys.argv[1]}")
@@ -43,8 +41,6 @@ def generate_content(client,messages):
         print(f"Response tokens: {usage.candidates_token_count}")
     print("Response:")    
     print(response.text)
-
-
 
 
 
